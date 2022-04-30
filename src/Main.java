8779cc66a2085b5993314772be19feffbb52051c
Андрей <andrews.ru83@gmail.com>
2022-04-30 12:03:39 +0300

    2.6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = 12 ;
+        int b = 27 ;
+        int c = 44 ;
+        int d = 15 ;
+        int e = 9 ;
+    int first = d * e ;
+    int second = c - first ;
+    int third = b + second ;
+    int result = a * third ;
+    result = - result ;
+
+        System.out.println(result);
     }
 }
